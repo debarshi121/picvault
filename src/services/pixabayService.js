4a269e4d2f1ec5c5aa1e888c36581da3181dad2c
Debarshi Das <debarshi121@gmail.com>
2024-01-21 11:17:09 +0530

    minor

--- src/services/pixabayService.js
+++ src/services/pixabayService.js
@@ -17,6 +17,16 @@ export const getBase64 = async (imageUrl) => {
 	}
 };
 
+export const addBlurredDataUrls = async (pixabayHits) => {
+	try {
+		const base64Promises = pixabayHits.map((obj) => getBase64(obj.webformatURL));
+		const base64Results = await Promise.all(base64Promises);
+		return base64Results;
+	} catch (error) {
+		throw error;
+	}
+};
+
 export const getSearchResults = async (query) => {
 	try {
 		const res = await axios.get(`https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${query}&image_type=photo&pretty=true`);
@@ -33,15 +43,5 @@ export const getSearchResults = async (query) => {
 		return dataWithblurredDataUrl;
 	} catch (error) {
 		throw new Error("Failed to fetch data");
-	}
-};
-
-export const addBlurredDataUrls = async (pixabayHits) => {
-	try {
-		const base64Promises = pixabayHits.map((obj) => getBase64(obj.webformatURL));
-		const base64Results = await Promise.all(base64Promises);
-		return base64Results;
-	} catch (error) {
-		throw error;
 	}
 };
