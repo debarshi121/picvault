6fd53f99f46b8e1bad3cae29d336fbe8c4cf5542
Debarshi Das <debarshi121@gmail.com>
2024-01-21 10:55:09 +0530

    aws4 module removed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
 	images: {
 		domains: ["cdn.pixabay.com", "pixabay.com"],
 	},
+	webpack: (config) => {
+		config.resolve.fallback = {
+			"mongodb-client-encryption": false,
+			aws4: false,
+		};
+
+		return config;
+	},
 };
 
 module.exports = nextConfig;
