b6cef420bc2a0e4132ef287fefbfa59573fbf768
Debarshi Das <debarshi121@gmail.com>
2024-01-21 11:24:28 +0530

    removed sharp package

--- src/components/trendingSearch/TrendingSearch.jsx
+++ src/components/trendingSearch/TrendingSearch.jsx
@@ -5,8 +5,8 @@ const TrendingSearch = async () => {
 	const data = await topKeywords();
 	return (
 		<div className="py-5 px-5 overflow-x-auto hide-scrollbar text-center space-x-2">
-			{data.map((item) => (
-				<Link href={`/search?q=${item.keyword}`} key={item.keyword} className="rounded text-sm border border-gray-500 text-gray-700 px-5 text-center py-1">
+			{data.map((item, i) => (
+				<Link href={`/search?q=${item.keyword}`} key={i} className="rounded text-sm border border-gray-500 text-gray-700 px-5 text-center py-1">
 					{item.keyword}
 				</Link>
 			))}
